cc6e73b838d794063f8834590209f9354a4d87fc
IvanProdaiko94 <prodaiko94@gmail.com>
2017-08-14 20:22:35 +0300

    working on koa example

--- examples/koa.js
+++ examples/koa.js
@@ -1,19 +1,62 @@
 'use strict';
 
 const Koa = require('koa');
+const Router = require('koa-router');
 const { DI } = require('..');
 
 const app = new Koa();
+const router = new Router();
 DI.integrateWith(app);
 
-const diMiddleware = async (ctx, next) => {
-  ctx.path()
-};
+class Ctrl {
+  static get $inject() {
+    return ['hello', 'world'];
+  }
+  constructor(s1, s2) {
+    this.s1 = s1;
+    this.s2 = s2;
+  }
 
-app.use(diMiddleware);
+  async greet(ctx) {
+    const timeout = ms => {
+      return new Promise(resolve => setTimeout(() => resolve(`${this.s1},${this.s2}`), ms));
+    };
+    ctx.body = await timeout(3000);
+  }
+}
 
-app.use(async ctx => {
 
+const servicesModule = app.module({
+  name: 'ServicesModule',
+  declarations: [
+    { name: 'hello', strategy: 'value', value: 'Hello' },
+    { name: 'world', strategy: 'value', value: 'World' },
+  ],
+  exports: '*'
 });
 
-app.listen(3000);
+app.bootstrap({
+  name: 'Module',
+  declarations: [
+    { name: 'Controller', strategy: 'singleton', value: Ctrl }
+  ],
+  imports: [ servicesModule ]
+});
+
+const pathToCtrl = {
+  '/': {
+    ctrl: 'Controller',
+    method: 'greet'
+  }
+};
+
+router.get('/', async ctx => {
+  const config = pathToCtrl[ctx._matchedRoute];
+  ctx.app.resolve(config.ctrl)[config.method](ctx);
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3001);
