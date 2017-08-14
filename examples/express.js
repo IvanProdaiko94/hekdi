1f28123b0a2cbcc5373f70caf3b0260810f3d151
IvanProdaiko94 <prodaiko94@gmail.com>
2017-08-14 20:04:08 +0300

    - express example written;

--- examples/express.js
+++ examples/express.js
@@ -1,2 +1,53 @@
 'use strict';
 
+const express = require('express');
+const { DI } = require('..');
+
+const di = DI.create();
+const app = express();
+
+class Ctrl {
+  static get $inject() {
+    return ['hello', 'world'];
+  }
+  constructor(s1, s2) {
+    this.s1 = s1;
+    this.s2 = s2;
+  }
+
+  greet(req, res) {
+    res.send(`${this.s1},${this.s2}`);
+  }
+}
+
+
+const servicesModule = di.module({
+  name: 'ServicesModule',
+  declarations: [
+    { name: 'hello', strategy: 'value', value: 'Hello' },
+    { name: 'world', strategy: 'value', value: 'World' },
+  ],
+  exports: '*'
+});
+
+di.bootstrap({
+  name: 'Module',
+  declarations: [
+    { name: 'Controller', strategy: 'singleton', value: Ctrl }
+  ],
+  imports: [ servicesModule ]
+});
+
+const pathToCtrl = {
+  '/': {
+    ctrl: 'Controller',
+    method: 'greet'
+  }
+};
+
+app.get('/', (req, res) => {
+  const config = pathToCtrl[req.path];
+  di.resolve(config.ctrl)[config.method](req, res);
+});
+
+app.listen(3000);
--- src/di.js
+++ src/di.js
@@ -1,35 +1,34 @@
 'use strict';
 const Module = require('./module');
 
-class DI {
-  constructor() {
-    this.main = null;
-  }
-
-  module(moduleConfig) {
-    if (!(moduleConfig instanceof Module)) {
-      return Module.createModule(moduleConfig);
-    }
-    return moduleConfig;
-  }
-
-  bootstrap(moduleConfig) {
-    this.main = this.module(moduleConfig);
-  }
-
-  resolve(dependencyName) {
-    return this.main.injector.resolve(dependencyName);
-  }
-
-  static create() {
-    return new DI();
-  }
-
-  static integrateWith(app) {
-    DI.call(app);
-    Object.assign(app, DI.prototype);
-    return app;
-  }
+function DI() {
+  this.main = null;
 }
+
+DI.prototype.module = function(moduleConfig) {
+  if (!(moduleConfig instanceof Module)) {
+    return Module.createModule(moduleConfig);
+  }
+  return moduleConfig;
+};
+
+DI.prototype.bootstrap = function(moduleConfig) {
+  this.main = this.module(moduleConfig);
+};
+
+
+DI.prototype.resolve = function(dependencyName) {
+  return this.main.injector.resolve(dependencyName);
+};
+
+DI.create = function() {
+  return new DI();
+};
+
+DI.integrateWith = function(app) {
+  DI.call(app);
+  Object.assign(app, DI.prototype);
+  return app;
+};
 
 module.exports = DI;
