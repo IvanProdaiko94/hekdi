c5c7786779a6a45b3f30f73a32851951bc35a4c4
IvanProdaiko94 <prodaiko94@gmail.com>
2017-10-22 16:52:36 +0300

    params order changed

--- src/utils/resolution_checker.js
+++ src/utils/resolution_checker.js
@@ -10,7 +10,7 @@ const resolutionChecker = function(arr = null, name, index = 0) {
   const toInject = this.getConfigOf(name).value.$inject;
   if (arr && arr.indexOf(name) !== -1) {
     arr.push(name);
-    throw new Error(circularDependency(arr.join(' -> '), name));
+    throw new Error(circularDependency(name, arr.join(' -> ')));
   }
   arr = arr || [];
   index ? arr.splice(index, 1, name) : arr.push(name);
